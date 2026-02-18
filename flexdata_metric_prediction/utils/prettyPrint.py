b640a62bf193bbc046b30f9a705456387cde95c8
Shaswat Gupta <shaswatgupta1702@gmail.com>
2026-02-18 15:04:43 +0100

    feat: Clean Up

--- flexdata_metric_prediction/utils/prettyPrint.py
+++
@@ -1,6 +0,0 @@
-def prettyPrintTree(root, ident):
-    print(ident + str(root) + "  {" + str(id(root)) + "}")
-    if ident == "":
-        ident = "|_"
-    for child in root.children:
-        prettyPrintTree(child, " " + ident)
